fafb8e64ccfbb36d34774a5b93f9ea0b3815c28e
Philippe <philippe.brunet@polymtl.ca>
2023-04-09 13:35:57 -0400

    better directory organization

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -12,10 +12,12 @@ sudo apt install -y default-jdk
 # Install Maven build tool
 sudo apt install -y maven
 
+cd mongo
+
 curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
 tar xfvz ycsb-0.17.0.tar.gz
 
 mkdir -p ycsb-0.17.0/database
-cp mongo/docker-compose-mongodb.yml ycsb-0.17.0/database/
+cp docker-compose-mongodb.yml ycsb-0.17.0/database/
 docker-compose -f ycsb-0.17.0/database/docker-compose-mongodb.yml up -d
 
