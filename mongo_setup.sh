98a23d80d8b034664872475cda0870b3e9397d51
Antoine Déry <antoinedery12@hotmail.fr>
2023-04-16 15:46:16 -0400

    added workloads modification in mongo_setup.sh

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -21,7 +21,7 @@ mkdir -p ycsb-0.17.0/database
 cp docker-compose-mongodb.yml ycsb-0.17.0/database/
 docker-compose -f ycsb-0.17.0/database/docker-compose-mongodb.yml up -d
 
-sleep 60
+sleep 30
 
 docker exec -it primary mongosh --eval 'rs.initiate({ _id: "myReplicaSet", members: [ {_id: 0, host: "primary"}, {_id: 1, host: "mongo1"}, {_id: 2, host: "mongo2"}, {_id: 3, host: "mongo3"} ] })'
 
@@ -29,3 +29,12 @@ sudo sh -c 'echo "192.168.5.2 primary" >> /etc/hosts'
 sudo sh -c 'echo "192.168.5.3 mongo1" >> /etc/hosts'
 sudo sh -c 'echo "192.168.5.4 mongo2" >> /etc/hosts'
 sudo sh -c 'echo "192.168.5.5 mongo3" >> /etc/hosts'
+
+cd "/home/ubuntu/TP3-LOG8430-Oscar/mongo/ycsb-0.17.0"
+
+for file in "/home/ubuntu/TP3-LOG8430-Oscar/mongo/ycsb-0.17.0/workloads"/workload{a,b,c,d,e,f}
+do
+    echo "fieldcount=3" >> "$file"
+    echo "fieldlength=100" >> "$file"
+    printf "Changing workloads in $file\n"
+done
